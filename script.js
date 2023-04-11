abb6a19c0d5f12e76e4bf8e31d6a55ae688036d5
Joozek3000 <j.jozefczyk90@gmail.com>
2023-04-11 19:46:40 +0100

    refactored to use classes

--- script.js
+++ script.js
@@ -1,105 +1,120 @@
-const modal = document.querySelector('.modal');
-const overlay = document.querySelector('.overlay');
-const closeBtn = document.querySelector('.close-modal');
-const newBookBtn = document.querySelector('.newBook');
-const addBook = document.querySelector('#addBtn');
-
-let myLibrary = [];
-let newBook;
-
 class Book {
   constructor(title, author, pages, read) {
-    this.title = form.title.value;
-    this.author = form.author.value;
-    this.pages = form.pages.value;
-    this.read = form.read.checked;
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
   }
 }
 
-//  Modal functionallity
-const closeModal = () => {
-  modal.classList.add('hidden');
-  overlay.classList.add('hidden');
-};
+class Library {
+  constructor() {
+    this.books = [];
+    this.modal = document.querySelector('.modal');
+    this.overlay = document.querySelector('.overlay');
+    this.closeBtn = document.querySelector('.close-modal');
+    this.newBookBtn = document.querySelector('.newBook');
+    this.addBookBtn = document.querySelector('#addBtn');
+    this.form = document.querySelector('form');
+    this.libraryElement = document.getElementById('library');
+    this.newBook = null;
 
-const openModal = () => {
-  modal.classList.remove('hidden');
-  overlay.classList.remove('hidden');
-};
+    this.closeModal = this.closeModal.bind(this);
+    this.openModal = this.openModal.bind(this);
+    this.addBookToLibrary = this.addBookToLibrary.bind(this);
 
-newBookBtn.addEventListener('click', openModal);
+    this.newBookBtn.addEventListener('click', this.openModal);
+    this.closeBtn.addEventListener('click', this.closeModal);
+    this.overlay.addEventListener('click', this.closeModal);
+    this.addBookBtn.addEventListener('click', this.addBookToLibrary);
+  }
 
-closeBtn.addEventListener('click', closeModal);
-overlay.addEventListener('click', closeModal);
+  openModal() {
+    this.modal.classList.remove('hidden');
+    this.overlay.classList.remove('hidden');
+  }
 
-//  adding book to library
-addBook.addEventListener('click', addBookToLibrary);
+  closeModal() {
+    this.modal.classList.add('hidden');
+    this.overlay.classList.add('hidden');
+  }
 
-function addBookToLibrary(event) {
-  event.preventDefault();
+  addBookToLibrary(event) {
+    event.preventDefault();
 
-  newBook = new Book(title, author, pages, read);
-  myLibrary.push(newBook);
-  form.reset();
-  closeModal();
-  render();
+    const form = this.form;
+    const newBook = new Book(
+      form.title.value,
+      form.author.value,
+      form.pages.value,
+      form.read.checked
+    );
+
+    this.books.push(newBook);
+    form.reset();
+    this.closeModal();
+    this.render();
+  }
+
+  removeBookFromLibrary(book) {
+    const index = this.books.indexOf(book);
+    if (index >= 0) {
+      this.books.splice(index, 1);
+      this.render();
+    }
+  }
+
+  render() {
+    while (this.libraryElement.firstChild) {
+      this.libraryElement.removeChild(this.libraryElement.firstChild);
+    }
+
+    for (const book of this.books) {
+      this.createBook(book);
+    }
+  }
+
+  createBook(book) {
+    const bookElement = document.createElement('div');
+    bookElement.classList.add('book');
+    bookElement.setAttribute('data-index', this.books.indexOf(book));
+
+    const titleElement = document.createElement('div');
+    titleElement.classList.add('title');
+    titleElement.textContent = book.title;
+    bookElement.appendChild(titleElement);
+
+    const authorElement = document.createElement('div');
+    authorElement.classList.add('author');
+    authorElement.textContent = book.author;
+    bookElement.appendChild(authorElement);
+
+    const pagesElement = document.createElement('div');
+    pagesElement.classList.add('pages');
+    pagesElement.textContent = `${book.pages} pages`;
+    bookElement.appendChild(pagesElement);
+
+    const readButton = document.createElement('button');
+    readButton.classList.add('readBtn');
+    readButton.textContent = book.read ? 'Read' : 'Not Read';
+    readButton.style.backgroundColor = book.read ? '#63da63' : '#e04f63';
+    readButton.addEventListener('click', () => {
+      book.read = !book.read;
+      this.render();
+    });
+    bookElement.appendChild(readButton);
+
+    const removeButton = document.createElement('button');
+    removeButton.classList.add('removeBtn');
+    removeButton.textContent = 'Remove';
+    removeButton.addEventListener('click', () => {
+      this.removeBookFromLibrary(book);
+    });
+    bookElement.appendChild(removeButton);
+
+    this.libraryElement.appendChild(bookElement);
+  }
 }
 
-const render = () => {
-  const display = document.getElementById('library');
-  const books = document.querySelectorAll('.book');
-  books.forEach((book) => display.removeChild(book));
-
-  for (let i = 0; i < myLibrary.length; i++) {
-    createBook(myLibrary[i]);
-  }
-};
-
-function createBook(item) {
-  const library = document.querySelector('#library');
-  const bookDiv = document.createElement('div');
-  const titleDiv = document.createElement('div');
-  const authorDiv = document.createElement('div');
-  const pagesDiv = document.createElement('div');
-  const readBtn = document.createElement('button');
-  const removeBtn = document.createElement('button');
-
-  bookDiv.classList.add('book');
-  bookDiv.setAttribute('id', myLibrary.indexOf(item));
-
-  titleDiv.textContent = item.title;
-  titleDiv.classList.add('title');
-  bookDiv.appendChild(titleDiv);
-
-  authorDiv.textContent = item.author;
-  authorDiv.classList.add('author');
-  bookDiv.appendChild(authorDiv);
-
-  pagesDiv.textContent = item.pages + ' pages';
-  pagesDiv.classList.add('pages');
-  bookDiv.appendChild(pagesDiv);
-
-  readBtn.classList.add('readBtn');
-  bookDiv.appendChild(readBtn);
-  if (item.read === false) {
-    readBtn.textContent = `Not Read`;
-    readBtn.style.backgroundColor = '#e04f63';
-  } else {
-    readBtn.textContent = `Read`;
-    readBtn.style.backgroundColor = '#63da63';
-  }
-  readBtn.addEventListener('click', () => {
-    item.read = !item.read;
-    render();
-  });
-
-  removeBtn.textContent = 'Remove';
-  removeBtn.classList.add('removeBtn');
-  bookDiv.appendChild(removeBtn);
-  removeBtn.addEventListener('click', () => {
-    myLibrary.splice(myLibrary.indexOf(item), 1);
-    render();
-  });
-
-  library.appendChild(bookDiv);
-}
+const library = new Library();
+library.render();
